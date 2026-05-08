7ae63abf12e9f37e3a5ae0517ca493a01ad3d7d2
Sagar Adhikari <sagradhkr48@gmail.com>
2026-05-08 17:44:53 +0545

    Validation imported with different schema validation rules

--- src/routes/business.routes.ts
+++ src/routes/business.routes.ts
@@ -4,11 +4,20 @@ import {
   createBusiness,
 } from "../controllers/business.controller.js";
 import { authMiddlewares } from "../middlewares/auth.middlewares.js";
+import { allowRoles } from "../middlewares/role.middlewares.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import { createBusinessSchema } from "../schema/business.schema.js";
 
 const router = Router();
 
 router.get("/businesses", getAllBusiness);
 
-router.post("/register", authMiddlewares, createBusiness);
+router.post(
+  "/register",
+  authMiddlewares,
+  allowRoles("OWNER", "ADMIN"),
+  validate(createBusinessSchema),
+  createBusiness,
+);
 
 export default router;
